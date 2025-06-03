4b9bcd0b9f1901372c105c3cfd757dc173bfa490
thegergi <gnihrizov@gmail.com>
2025-06-03 22:38:13 +0200

    EUDI verification flow works and can extract user attributes

--- offlineeuro/src/main/java/nl/tudelft/trustchain/offlineeuro/ui/AllRolesFragment.kt
+++ offlineeuro/src/main/java/nl/tudelft/trustchain/offlineeuro/ui/AllRolesFragment.kt
@@ -1,7 +1,6 @@
 package nl.tudelft.trustchain.offlineeuro.ui
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -28,6 +27,7 @@ import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 import androidx.core.net.toUri
+import org.json.JSONArray
 
 class AllRolesFragment : OfflineEuroBaseFragment(R.layout.fragment_all_roles_home) {
     private lateinit var iPV8CommunicationProtocol: IPV8CommunicationProtocol
@@ -39,8 +39,8 @@ class AllRolesFragment : OfflineEuroBaseFragment(R.layout.fragment_all_roles_hom
 
     private val mainScope = CoroutineScope(Dispatchers.Main + Job())
     private val client = OkHttpClient()
-
-
+    private val presentationURL = "https://verifier-backend.eudiw.dev/ui/presentations"
+    private val valdiationURL = "https://verifier-backend.eudiw.dev/utilities/validations/msoMdoc/deviceResponse"
 
     override fun onViewCreated(
         view: View,
@@ -119,8 +119,6 @@ class AllRolesFragment : OfflineEuroBaseFragment(R.layout.fragment_all_roles_hom
         }
 
         eudiButton.setOnClickListener {
-
-            val presentationURL = "https://verifier-backend.eudiw.dev/ui/presentations"
             val body = """
                 {
                   "type": "vp_token",
@@ -160,17 +158,13 @@ class AllRolesFragment : OfflineEuroBaseFragment(R.layout.fragment_all_roles_hom
 
             mainScope.launch {
                 // Start verifier transaction
-                val result = makeAPIRequest(presentationURL, body)
-
-                if (result == "") {
-                    return@launch
-                }
+                val result = makeAPIRequest(presentationURL, body, "application/json; charset=utf-8".toMediaType()) ?: return@launch
 
                 // Parse JSON
                 val jsonObject = JSONObject(result)
-                val transactionId = jsonObject.optString("transaction_id", null)
-                val clientId = jsonObject.optString("client_id", null)
-                val requestURI = jsonObject.optString("request_uri", null)
+                val transactionId = jsonObject.optString("transaction_id")
+                val clientId = jsonObject.optString("client_id")
+                val requestURI = jsonObject.optString("request_uri")
                 val requestURIMethod = "get"
                 Log.d("EUDI", "Transaction ID: $transactionId")
                 Log.d("EUDI", "Transaction ID: $requestURI")
@@ -184,34 +178,108 @@ class AllRolesFragment : OfflineEuroBaseFragment(R.layout.fragment_all_roles_hom
                 }
 
                 startActivity(intent)
+
+                val vpToken = getVPToken(transactionId)  ?: return@launch
+
+                val attributes = validateVPToken(vpToken) ?: return@launch
+
+                val familyName = attributes.optString("family_name")
+
+                Log.d("EUDI", "SUCESSS Family name: $familyName")
+
             }
         }
     }
 
+    private suspend fun getVPToken(transactionId: String): String? {
+        var currentAttempt = 0
+        val maxRetries = 1000
+        val delayBetweenRetries = 20L
 
-    private suspend fun makeAPIRequest(url: String, jsonBody: String): String {
+        while (currentAttempt < maxRetries) {
+            try {
+                Log.d("EUDI", "Attempting to get VP token, attempt ${currentAttempt + 1}/$maxRetries")
+
+                val vpToken = requestVPToken(transactionId)
+                if (!vpToken.isNullOrEmpty()) {
+                    // Success! Process the token
+                    Log.d("EUDI", "VP token received successfully")
+                    return vpToken
+                }
+            } catch (e: Exception) {
+                Log.w("EUDI", "Attempt ${currentAttempt + 1} failed: ${e.message}")
+            }
+
+            currentAttempt++
+
+            if (currentAttempt < maxRetries) {
+                Log.d("EUDI", "Waiting ${delayBetweenRetries}ms before next attempt...")
+                delay(delayBetweenRetries)
+            }
+        }
+
+        Log.e("EUDI", "Failed to get VP token after $maxRetries attempts")
+        return null
+    }
+
+    private suspend fun requestVPToken(transactionId: String): String? {
+        Log.d("EUDI", "Requesting vp token...")
+        val result = makeAPIRequest("$presentationURL/$transactionId", "", "application/json; charset=utf-8".toMediaType()) ?: return null
+
+        // Parse JSON
+        val jsonObject = JSONObject(result)
+        val vpToken = jsonObject.optJSONArray("vp_token")?.optString(0, null)
+
+        Log.d("EUDI", "VP token: $vpToken")
+
+        return vpToken
+    }
+
+    private suspend fun validateVPToken(vpToken: String): JSONObject? {
+        Log.d("EUDI", "Validation vp token...")
+        val body = "device_response=$vpToken"
+        val result = makeAPIRequest(valdiationURL, body, "application/x-www-form-urlencoded".toMediaType()) ?: return null
+
+        // Parse JSON
+        val jsonObject = JSONArray(result).optJSONObject(0)
+        val docType = jsonObject?.optString("docType")
+        val attributes = jsonObject?.optJSONObject("attributes")?.optJSONObject(docType)
+
+        Log.d("EUDI", "Data: $attributes")
+
+        return attributes
+    }
+
+    private suspend fun makeAPIRequest(url: String, body: String, mediaType: MediaType): String? {
         Log.d("EUDI", "Calling api endpoint: $url")
 
         return withContext(Dispatchers.IO) {
             try {
-                val mediaType = "application/json; charset=utf-8".toMediaType()
-                val requestBody = jsonBody.toRequestBody(mediaType)
+                val request = if (body != "") {
+                    val requestBody = body.toRequestBody(mediaType)
 
-                val request = Request.Builder()
-                    .url(url)
-                    .post(requestBody)
-                    .build()
+                    Request.Builder()
+                        .url(url)
+                        .post(requestBody)
+                        .build()
+                } else {
+                    Request.Builder()
+                        .url(url)
+                        .get()
+                        .build()
+                }
 
                 client.newCall(request).execute().use { response ->
                     if (!response.isSuccessful) {
-                        Log.e("API_CALL", "Unsuccessful response: ${response.code}")
-                        return@withContext ""
+                        Log.e("EUDI", "Unsuccessful response: ${response.code}")
+                        Log.e("EUDI", "Unsuccessful response: ${response.body?.string()}")
+                        return@withContext null
                     }
 
                     val responseBodyString = response.body?.string()
                     if (responseBodyString.isNullOrEmpty()) {
                         Log.e("EUDI", "Empty response body")
-                        return@withContext ""
+                        return@withContext null
                     }
 
                     Log.d("EUDI", "Response: $responseBodyString")
@@ -220,13 +288,13 @@ class AllRolesFragment : OfflineEuroBaseFragment(R.layout.fragment_all_roles_hom
                 }
             } catch (e: IOException) {
                 Log.e("EUDI", "IOException: ${e.message}", e)
-                return@withContext ""
+                return@withContext null
             } catch (e: JSONException) {
                 Log.e("EUDI", "JSON parsing error: ${e.message}", e)
-                return@withContext ""
+                return@withContext null
             } catch (e: Exception) {
                 Log.e("EUDI", "Exception: ${e.message}", e)
-                return@withContext ""
+                return@withContext null
             }
         }
     }
